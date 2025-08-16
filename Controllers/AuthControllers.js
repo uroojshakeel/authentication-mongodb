4cb69f9c240e3b9d5a2bbca12867534515a2bd0a
Urooj Shakeel <165478020+uroojshakeel@users.noreply.github.com>
2025-08-16 08:49:54 +0500

    Update AuthControllers.js

--- Controllers/AuthControllers.js
+++ Controllers/AuthControllers.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const UserModel = require("../Models/user");
+const UserModel = require("../Models/User");
 
 
 const signup = async (req, res) => {
